0d08142d4274e8f27bec04e920a8c43bc7b4251b
MattDahEpic <contact@mattdahepic.com>
2017-04-02 10:20:07 -0600

    resolve #10

--- src/main/java/com/mattdahepic/ringkydinks/item/ItemDink.java
+++ src/main/java/com/mattdahepic/ringkydinks/item/ItemDink.java
@@ -3,7 +3,6 @@ package com.mattdahepic.ringkydinks.item;
 import com.mattdahepic.ringkydinks.RingkyDinks;
 import com.mattdahepic.ringkydinks.dink.DinkNBT;
 import com.mattdahepic.ringkydinks.dink.EnumDink;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -18,7 +17,7 @@ public class ItemDink extends Item {
         this.setCreativeTab(RingkyDinks.tab);
     }
     public String getItemStackDisplayName(ItemStack stack) {
-        return DinkNBT.getNiceDinkNameForTooltip(DinkNBT.getDinkType(stack))+I18n.format("item.dink.suffix");
+        return DinkNBT.getNiceDinkNameForTooltip(DinkNBT.getDinkType(stack))+" Dink";
     }
     @Override
     public void getSubItems(Item item, CreativeTabs tab, List<ItemStack> subItems) {
